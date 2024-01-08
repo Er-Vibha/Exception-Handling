97a85f505d72ff7bed2670a8e11cdae37b46f96c
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2024-01-08 16:51:09 +0530

    Create finallyEx.java

---
+++ finallyEx.java
@@ -0,0 +1,15 @@
+public class finallyEx {
+    public static void main(String[] args) {
+        try{
+            int a[]=new int[5];
+            a[5]=4;
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error Catched");
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("This is finally block ");
+        }
+    }
+}
